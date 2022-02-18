774ff42e2189992032d568be31be3c6d28c03a32
Jorge Solis Prado <yonodibujo@gmail.com>
2022-02-18 16:37:43 -0300

    delete items

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1>Mi lista de tareas</h1>
+    <form v-on:submit.prevent="agregarTarea">
+      <label>Nueva Tarea</label>
+      <input type="text" v-model="nombre" />
+      <button class="btn" type="submit">Agregar Tarea</button>
+    </form>
+
+    <h2>Esta es mi lista de tareas .</h2>
+
+    <HelloWorld :tareas="this.tareas" @deleteItem="deleteItem"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      nombre: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    agregarTarea: function () {
+      this.tareas.push({
+        nombre: this.nombre,
+      })
+      this.nombre = ""
+    },
+    deleteItem(index){
+      this.tareas.splice(index, 1)
+    }
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+label{
+  font-size: 18px;
+  text-transform: uppercase;
+  margin-right: 1rem;
+}
+input{
+  width: 200px;
+  height: 25px;
+  margin-right: 1rem;
+}
+.btn{
+  height: 2rem;
+
+}
+body{
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+   <ul v-for="(tarea, index) in tareas" :key="tarea">
+      <li><span>{{ index+1 }} - {{ tarea.nombre }}</span>  <button @click="deleteItem(index)">Eliminar x</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    tareas: Array
+  },
+  methods:{
+    deleteItem(index){
+      this.$emit("deleteItem", index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul{
+    display: flex;
+    justify-content: center;
+  }
+  span{
+    font-size: 18px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    margin-left: 1rem;
+  }
+  button{
+    width: 90px;
+    height: 2rem;
+    background: rgb(243, 69, 69);
+    color: #fff;
+    border-radius: 0.3rem;
+    margin-left: 1rem;
+    border: none;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-transform: uppercase;
+  }
+</style>
